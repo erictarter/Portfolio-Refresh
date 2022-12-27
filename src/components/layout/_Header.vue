2c9e119717dc033c480be471bc5903c8e3e11237
Eric Tarter <etarter@ccli.com>
2022-12-27 13:07:52 -0800

    initial setup vue3/sass/bs5

---
+++ src/components/layout/_Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+      <div class="navbar">
+            <div class="brand">my brand</div>
+            <div class="nav-items">
+                  <a class="nav-item" href="#">Bio</a>
+                  <a class="nav-item" href="#">Projects</a>
+                  <a class="nav-item" href="#">Blog</a>
+                  <a class="nav-item" href="#">Socials</a>
+            </div>
+      </div>
+</template>
+
+<style scoped>
+</style>
