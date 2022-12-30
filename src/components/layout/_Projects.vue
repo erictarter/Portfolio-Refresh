8f9e505f257665d5093fae11e437f1e7a9727ad0
Eric Tarter <etarter@ccli.com>
2022-12-30 15:29:04 -0800

    project description

--- src/components/layout/_Projects.vue
+++ src/components/layout/_Projects.vue
@@ -10,13 +10,29 @@ import { defineComponent } from 'vue'
         <h2 class="mb-4">Projects</h2>
             <div class="projects">
             <a target="_blank" href="https://goofy-ritchie-d401d6.netlify.app/" class="project">
-              <!-- <p class="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit, a tenetur nobis corrupti hic?</p> -->
+              <p class="fs-5 project-description-lg d-none d-md-flex">Sports Betting App. Fill out a betslip and place a hypothetical bet</p>
+              <p class="project-description-mobile d-flex d-md-none">Sports Betting App. Fill out a betslip and place a hypothetical bet</p>
             </a>
-            <a target="_blank" href="https://mystifying-mahavira-4e8faa.netlify.app/" class="project"></a>
-            <a target="_blank" href="https://gifted-kepler-32c70b.netlify.app/" class="project"></a>
-            <a target="_blank" href="https://tender-feynman-0a5def.netlify.app/" class="project"></a>
-            <a target="_blank" href="https://heuristic-newton-d525bc.netlify.app/" class="project"></a>
-            <a target="_blank" href="https://affectionate-booth-7c6f42.netlify.app/" class="project"></a>
+            <a target="_blank" href="https://mystifying-mahavira-4e8faa.netlify.app/" class="project">
+              <p class="fs-5 project-description-lg d-none d-md-flex">Online Sunglasses Store with shopping cart</p>
+              <p class="project-description-mobile d-flex d-md-none">Online Sunglasses Store with shopping cart</p>
+            </a>
+            <a target="_blank" href="https://gifted-kepler-32c70b.netlify.app/" class="project">
+              <p class="fs-5 project-description-lg d-none d-md-flex">Find Out how much house you can afford in this questionaire</p>
+              <p class="project-description-mobile d-flex d-md-none">Find Out how much house you can afford in this questionaire</p>            
+            </a>
+            <a target="_blank" href="https://tender-feynman-0a5def.netlify.app/" class="project">
+              <p class="fs-5 project-description-lg d-none d-md-flex">Get up to date covid data from the Covid Tracking Data API</p>
+              <p class="project-description-mobile d-flex d-md-none">Get up to date covid data from the Covid Tracking Data API</p>              
+            </a>
+            <a target="_blank" href="https://heuristic-newton-d525bc.netlify.app/" class="project">
+              <p class="fs-5 project-description-lg d-none d-md-flex">Test your NBA knowledge with this quiz</p>
+              <p class="project-description-mobile d-flex d-md-none">Test your NBA knowledge with this quiz</p>             
+            </a>
+            <a target="_blank" href="https://affectionate-booth-7c6f42.netlify.app/" class="project">
+              <p class="fs-5 project-description-lg d-none d-md-flex">Play around on this Tone JS Piano App</p>
+              <p class="project-description-mobile d-flex d-md-none">Play around on this Tone JS Piano App</p>   
+            </a>
           </div>
       </div>
 </template>
