c83c52f0bbbc8a3c9e4b8dff23d72ca2a648db96
Eric Tarter <etarter@ccli.com>
2022-12-31 13:21:15 -0800

    show more projects function added

--- src/components/layout/_Projects.vue
+++ src/components/layout/_Projects.vue
@@ -9,6 +9,12 @@ import { defineComponent } from 'vue'
       },
       hideOverlay(e:any){
         e.target.children[0].classList.remove('project-overlay-show')
+      },
+      showMoreProjects(e:any) {
+        document.querySelectorAll('.more-project').forEach(i => {
+          i.classList.add('more-project-show')
+        })
+        e.target.classList.add('hide-btn')
       }
     }
 })
@@ -33,17 +39,18 @@ import { defineComponent } from 'vue'
               <p class="fs-5 project-description-lg d-none px-3 d-md-flex">Find Out how much house you can afford in this questionaire</p>
               <p class="project-description-mobile d-flex d-md-none p-2">Home Budget</p>            
             </a>
-            <a @mouseenter="showOverlay" @mouseleave="hideOverlay" target="_blank" href="https://tender-feynman-0a5def.netlify.app/" class="project">
+            <a href="#/#" @click='showMoreProjects' class="show-more-projects btn d-none p-2 fs-6">Show More</a>
+            <a @mouseenter="showOverlay" @mouseleave="hideOverlay" target="_blank" href="https://tender-feynman-0a5def.netlify.app/" class="project more-project" id="/#">
               <div class="project-overlay d-none d-md-flex"></div>
               <p class="fs-5 project-description-lg px-3 d-none d-md-flex">Get up to date covid data from the Covid Tracking Data API</p>
               <p class="project-description-mobile d-flex d-md-none p-2">Covid Data</p>              
             </a>
-            <a @mouseenter="showOverlay" @mouseleave="hideOverlay" target="_blank" href="https://heuristic-newton-d525bc.netlify.app/" class="project">
+            <a @mouseenter="showOverlay" @mouseleave="hideOverlay" target="_blank" href="https://heuristic-newton-d525bc.netlify.app/" class="project more-project">
               <div class="project-overlay d-none d-md-flex"></div>
               <p class="fs-5 project-description-lg d-none px-3 d-md-flex">Test your NBA knowledge with this quiz</p>
               <p class="project-description-mobile d-flex d-md-none p-2">NBA Quiz</p>             
             </a>
-            <a @mouseenter="showOverlay" @mouseleave="hideOverlay" target="_blank" href="https://affectionate-booth-7c6f42.netlify.app/" class="project">
+            <a @mouseenter="showOverlay" @mouseleave="hideOverlay" target="_blank" href="https://affectionate-booth-7c6f42.netlify.app/" class="project more-project">
               <div class="project-overlay d-none d-md-flex"></div>
               <p class="fs-5 project-description-lg d-none px-3 d-md-flex">Play around on this Tone JS Piano App</p>
               <p class="project-description-mobile d-flex d-md-none p-2">Tone JS Piano App</p>   
