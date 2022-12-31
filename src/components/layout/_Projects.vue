b462de3748dd2f62a438fb8784378f9114e5f2e0
Eric Tarter <etarter@ccli.com>
2022-12-31 07:25:33 -0800

    project text change

--- src/components/layout/_Projects.vue
+++ src/components/layout/_Projects.vue
@@ -10,28 +10,34 @@ import { defineComponent } from 'vue'
         <h2 class="mb-4">Projects</h2>
             <div class="projects">
             <a target="_blank" href="https://goofy-ritchie-d401d6.netlify.app/" class="project">
-              <p class="fs-5 project-description-lg d-none d-md-flex">Sports Betting App. Fill out a betslip and place a hypothetical bet</p>
-              <p class="project-description-mobile d-flex d-md-none">Sports Betting App. Fill out a betslip and place a hypothetical bet</p>
+              <div class="project-overlay"></div>
+              <p class="fs-5 project-description-lg d-none px-3 d-md-flex">Sports Betting App. Fill out a betslip and place a hypothetical bet</p>
+              <p class="project-description-mobile d-flex d-md-none">Sports Betting App</p>
             </a>
             <a target="_blank" href="https://mystifying-mahavira-4e8faa.netlify.app/" class="project">
-              <p class="fs-5 project-description-lg d-none d-md-flex">Online Sunglasses Store with shopping cart</p>
-              <p class="project-description-mobile d-flex d-md-none">Online Sunglasses Store with shopping cart</p>
+              <div class="project-overlay"></div>
+              <p class="fs-5 project-description-lg d-none px-3 d-md-flex">Online Sunglasses Store with shopping cart</p>
+              <p class="project-description-mobile d-flex d-md-none">Online Store W/shopping cart</p>
             </a>
             <a target="_blank" href="https://gifted-kepler-32c70b.netlify.app/" class="project">
-              <p class="fs-5 project-description-lg d-none d-md-flex">Find Out how much house you can afford in this questionaire</p>
-              <p class="project-description-mobile d-flex d-md-none">Find Out how much house you can afford in this questionaire</p>            
+              <div class="project-overlay"></div>
+              <p class="fs-5 project-description-lg d-none px-3 d-md-flex">Find Out how much house you can afford in this questionaire</p>
+              <p class="project-description-mobile d-flex d-md-none">Home Budget</p>            
             </a>
             <a target="_blank" href="https://tender-feynman-0a5def.netlify.app/" class="project">
-              <p class="fs-5 project-description-lg d-none d-md-flex">Get up to date covid data from the Covid Tracking Data API</p>
-              <p class="project-description-mobile d-flex d-md-none">Get up to date covid data from the Covid Tracking Data API</p>              
+              <div class="project-overlay"></div>
+              <p class="fs-5 project-description-lg px-3 d-none d-md-flex">Get up to date covid data from the Covid Tracking Data API</p>
+              <p class="project-description-mobile d-flex d-md-none">Covid Data</p>              
             </a>
             <a target="_blank" href="https://heuristic-newton-d525bc.netlify.app/" class="project">
-              <p class="fs-5 project-description-lg d-none d-md-flex">Test your NBA knowledge with this quiz</p>
-              <p class="project-description-mobile d-flex d-md-none">Test your NBA knowledge with this quiz</p>             
+              <div class="project-overlay"></div>
+              <p class="fs-5 project-description-lg d-none px-3 d-md-flex">Test your NBA knowledge with this quiz</p>
+              <p class="project-description-mobile d-flex d-md-none">NBA Quiz</p>             
             </a>
             <a target="_blank" href="https://affectionate-booth-7c6f42.netlify.app/" class="project">
-              <p class="fs-5 project-description-lg d-none d-md-flex">Play around on this Tone JS Piano App</p>
-              <p class="project-description-mobile d-flex d-md-none">Play around on this Tone JS Piano App</p>   
+              <div class="project-overlay"></div>
+              <p class="fs-5 project-description-lg d-none px-3 d-md-flex">Play around on this Tone JS Piano App</p>
+              <p class="project-description-mobile d-flex d-md-none">Tone JS Piano App</p>   
             </a>
           </div>
       </div>
