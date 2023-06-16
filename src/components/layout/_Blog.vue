39f99a324b8b1648dbf444d26e4a6eca1bd35f32
Eric Tarter <etarter@ccli.com>
2023-06-16 15:37:56 -0700

    switch blog link

--- src/components/layout/_Blog.vue
+++ src/components/layout/_Blog.vue
@@ -1,18 +1,20 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+  import { defineComponent } from 'vue'
 
-  export default defineComponent({
-})
+  export default defineComponent({})
 </script>
 
 <template>
-      <div class="blog-section d-flex justify-content-around justify-content-lg-center align-items-center my-3 p-3">
-        <div class="w-50">
-            <h2 class=''>Check Out My Blog Post</h2>
-        </div>
-            <a target="_blank" href="https://musing-gates-809017.netlify.app/" class="blog-link"></a>
-      </div>
+  <div
+    class="blog-section d-flex justify-content-around justify-content-lg-center align-items-center my-3 p-3">
+    <div class="w-50">
+      <h2 class="">Check Out My Blog Post</h2>
+    </div>
+    <a
+      target="_blank"
+      href="https://timely-semolina-84be97.netlify.app/"
+      class="blog-link"></a>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
