48342bff637756c8ebb078800ad9ddc55ecb929d
Eric Tarter <etarter@ccli.com>
2023-06-27 09:34:08 -0700

    add Vue to bio

--- src/components/layout/_Bio.vue
+++ src/components/layout/_Bio.vue
@@ -1,19 +1,22 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+  import { defineComponent } from 'vue'
 
-  export default defineComponent({
-})
+  export default defineComponent({})
 </script>
 
 <template>
-      <div class="bio d-flex flex-column justify-content-center p-5 pt-0 pt-md-5" id="/$">
-        <div class="bio-personal">
-          <h3>Eric Tarter</h3>
-          <h1>Front-End Developer</h1>
-          <p class="fs-5">Hi, my name is Eric Tarter. I am a Front-End Web Developer Living in the Pacific Northwest.</p>
-        </div>
-      </div>
+  <div
+    class="bio d-flex flex-column justify-content-center p-5 pt-0 pt-md-5"
+    id="/$">
+    <div class="bio-personal">
+      <h3>Eric Tarter</h3>
+      <h1>Front-End Developer</h1>
+      <p class="fs-5">
+        Hi, my name is Eric Tarter. I am a Vue Developer Living in the Pacific
+        Northwest.
+      </p>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
