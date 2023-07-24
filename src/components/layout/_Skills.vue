cc6887e8e7ad1d0b7d3cc246d8f5aef8c584c397
Eric Tarter <etarter@ccli.com>
2023-07-24 15:16:11 -0700

    added glow to skill bar

--- src/components/layout/_Skills.vue
+++ src/components/layout/_Skills.vue
@@ -1,11 +1,12 @@
-<script lang="ts">
-  import { defineComponent } from 'vue'
-
-  export default defineComponent({})
+<script setup lang="ts">
+  defineProps<{
+    darkMode: boolean
+  }>()
 </script>
 
 <template>
   <div class="skills-section mt-5 mx-5">
+    <div v-if="darkMode" class="shiny"></div>
     <div
       class="skills d-flex justify-content-between align-items-center px-2 mt-4">
       <v-icon class="mx-md-3 mx-2 skill-icon" name="io-logo-vue" />
@@ -18,4 +19,13 @@
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .shiny {
+    width: 100%;
+    height: 2.8rem;
+    position: absolute;
+    background-color: hsla(330deg, 95%, 83%, 0.3);
+    border-radius: 6px;
+    box-shadow: 0 0 14px 0 rgb(255, 255, 255);
+  }
+</style>
